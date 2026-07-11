b9deb7218b6bbfd2cf842abe9a046ae08e5f75e2
Deepshikha YAdav <deepshikhay920@gmail.com>
2026-07-10 18:53:32 -0700

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-let btn=document.querySelector("button");
-btn.addEventListener("click",function(){
-    console.log("generate randon color");
-    let h3=document.querySelector("h3");
-    let rndmclr=getRandomColor();
-    h3.innerText=rndmclr;
-    console.log("Color updated");
-    let div=document.querySelector("div");
-    div.style.backgroundColor=rndmclr;
-});
-function getRandomColor(){
-    let red=Math.floor(Math.random()*255);
-    let green=Math.floor(Math.random()*255);
-    let blue=Math.floor(Math.random()*255);
-    let clr=`rgb(${red},${green},${blue})`;
-    return clr;
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+btn.addEventListener("click", function () {
+   let item = document.createElement("li");
+   item.innerText = inp.value;
+   ul.appendChild(item);
+   inp.value = "";
 
-}
+   let delBtn = document.createElement("button");
+   delBtn.innerText = "Delete";
+   delBtn.classList.add("delete");
+   item.appendChild(delBtn);
+   
+});
+ul.addEventListener("click",function(event){
+   if(event.target.nodeName=="BUTTON"){
+      let listItem=event.target.parentElement;
+      listItem.remove();
+      console.log("deleted");
+   }
+});
+
+
+
